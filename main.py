ef6356cc313710f9106afb87d5d0699ac31b8664
Stepan Fedorov <huanandeluxe@yandex.ru>
2021-04-11 16:42:12 +0000

    Основные пути и структура файлов

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('Initial commit')
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'главная страница'
+
+
+@app.route('/register')
+def register():
+    return 'регистрация'
+
+
+@app.route('/login')
+def login():
+    return 'логин'
+
+
+@app.route('/notes')
+def notes():
+    return 'заметки'
+
+
+@app.route('/notes/<int:id>')
+def note_by_id(id: int):
+    return 'заметка по ID'
+
+
+if __name__ == '__main__':
+    app.run()
