424d5fd6ad0328e03958e2191cbf803d331dbfa0
Stepan Fedorov <huanandeluxe@yandex.ru>
2021-04-16 16:31:46 +0000

    HTML шаблоны, стили

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
@@ -6,27 +6,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'главная страница'
+    return render_template('main.html')
 
 
 @app.route('/register')
 def register():
-    return 'регистрация'
+    return render_template('register.html')
 
 
 @app.route('/login')
 def login():
-    return 'логин'
+    return render_template('login.html')
 
 
 @app.route('/notes')
 def notes():
-    return 'заметки'
+    return render_template('notes.html', notes=[{"title": "Название", "text": "обязательно нужно это сделать " * 3}])
 
 
 @app.route('/notes/<int:id>')
 def note_by_id(id: int):
     return 'заметка по ID'
+
+
+@app.route('/styles.css')
+def styles():
+    return app.send_static_file('styles.css')
 
 
 if __name__ == '__main__':
